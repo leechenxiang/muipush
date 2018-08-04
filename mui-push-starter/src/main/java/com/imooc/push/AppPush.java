a3bef309ec3f128b5594e28deee426e3cf6cf866
lee <lee@leechenxiang>
2018-08-04 15:02:53 +0800

    慕课网《mui+个推实现混合app推送》

---
+++ mui-push-starter/src/main/java/com/imooc/push/AppPush.java
@@ -0,0 +1,79 @@
+package com.imooc.push;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gexin.rp.sdk.base.IPushResult;
+import com.gexin.rp.sdk.base.impl.AppMessage;
+import com.gexin.rp.sdk.http.IGtPush;
+import com.gexin.rp.sdk.template.LinkTemplate;
+import com.gexin.rp.sdk.template.NotificationTemplate;
+import com.gexin.rp.sdk.template.style.Style0;
+
+
+public class AppPush {
+
+    //定义常量, appId、appKey、masterSecret 采用本文档 "第二步 获取访问凭证 "中获得的应用配置
+    private static String appId = "fBKq6G78P16o6Ef8NjM3X2";
+    private static String appKey = "dnWe0IZQTYAJzciX63Qy88";
+    private static String masterSecret = "3IZB5F7jtK9jNl0r0QM998";
+    private static String url = "http://sdk.open.api.igexin.com/apiex.htm";
+
+    public static void main(String[] args) throws IOException {
+
+        IGtPush push = new IGtPush(url, appKey, masterSecret);
+
+        // 定义"点击链接打开通知模板"，并设置标题、内容、链接
+//        LinkTemplate template = new LinkTemplate();
+//        template.setAppId(appId);
+//        template.setAppkey(appKey);
+//        template.setTitle("慕课网");
+//        template.setText("打开慕课网官方网址");
+//        template.setUrl("https://www.imooc.com");
+        
+        
+        NotificationTemplate template = notificationTemplateDemo(appId, appKey);
+
+        List<String> appIds = new ArrayList<String>();
+        appIds.add(appId);
+
+        // 定义"AppMessage"类型消息对象，设置消息内容模板、发送的目标App列表、是否支持离线发送、以及离线消息有效期(单位毫秒)
+        AppMessage message = new AppMessage();
+        message.setData(template);
+        message.setAppIdList(appIds);
+        message.setOffline(true);
+        message.setOfflineExpireTime(1000 * 600);
+
+        IPushResult ret = push.pushMessageToApp(message);
+        System.out.println(ret.getResponse().toString());
+    }
+    
+    public static NotificationTemplate notificationTemplateDemo(String appId, String appkey) {
+        NotificationTemplate template = new NotificationTemplate();
+        // 设置APPID与APPKEY
+        template.setAppId(appId);
+        template.setAppkey(appkey);
+        // 透传消息设置，1为强制启动应用，客户端接收到消息后就会立即启动应用；2为等待应用启动
+        template.setTransmissionType(1);
+        template.setTransmissionContent("文字消息通知");
+        // 设置定时展示时间
+        // template.setDuration("2015-01-16 11:40:00", "2015-01-16 12:24:00");
+
+        Style0 style = new Style0();
+        // 设置通知栏标题与内容
+        style.setTitle("文字消息通知");
+        style.setText("文字消息通知内容");
+        // 配置通知栏图标
+        style.setLogo("icon.png");
+        // 配置通知栏网络图标
+        style.setLogoUrl("");
+        // 设置通知是否响铃，震动，或者可清除
+        style.setRing(true);
+        style.setVibrate(true);
+        style.setClearable(true);
+        template.setStyle(style);
+
+        return template;
+    }
+}
